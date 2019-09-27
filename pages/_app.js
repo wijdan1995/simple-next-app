53ab55fa1afb79c905040b4bd46d5c0c9f05974f
wijdan1995 <wijdan.w.k@outlook.sa>
2019-09-27 22:41:08 +0300

    Setup redux and react redux

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import App from 'next/app'
 import { appWithTranslation } from '../i18n'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from '../reducers/rootReducer'
 
+const store = createStore(rootReducer);
 class MyApp extends App {
     render() {
         const { Component, pageProps } = this.props
-        return <Component {...pageProps} />
+        return (
+            <Provider store={store}>
+                <Component {...pageProps} />
+            </Provider>)
     }
 }
 
