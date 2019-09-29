d0ecee2f3eaffa27c8924b9059bfdfd7320ed63e
wijdan1995 <wijdan.w.k@outlook.sa>
2019-09-29 14:59:48 +0300

    connect it with the backend

--- actions/contactAction.js
+++ actions/contactAction.js
@@ -1,6 +1,24 @@
+const axios = require('axios')
+
+const apiUrl = 'http://localhost:1234';
+
 export const sendMessage = (message) => {
-    return (dispatch, getState) => {
-        // async call
-        dispatch({ type: 'SEND_MESSAGE', message })
+    return (dispatch) => {
+
+        axios.post(`${apiUrl}/contact`, message
+        )
+            .then(response => {
+                dispatch({ type: 'SEND_MESSAGE', message })
+                console.log(response);
+            })
+            .catch(error => {
+                throw (error);
+            });
+
+
+        // dispatch({ type: 'SEND_MESSAGE', message })
+
+
+
     }
 }
